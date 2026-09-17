7cfaedd9a94ac40d3ae9fa519a09c3a919a505b3
ThanhThuyNguye <157895529+ThanhThuyNguye@users.noreply.github.com>
2026-09-17 13:08:46 +0200

    Inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ function refreshWeather(response) {
               />`;
 }
 
-
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -56,7 +55,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong> 15°</strong>
+      </div>
+      <div class="weather-forecast-temperature">9°</div>
+    </div>
+  </div>
+     `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
